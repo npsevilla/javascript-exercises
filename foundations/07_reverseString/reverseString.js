74e45f24022e5e71075b7918d23ac4e332fcfc21
npsevilla <220456424+npsevilla@users.noreply.github.com>
2026-06-21 07:09:54 +0700

    Complete 07_reverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let reverseString = "";
+  for (i = 0; i < string.length; i++) {
+    reverseString += string[string.length - 1 - i];
+  }
+  return reverseString;
 };
 
 // Do not edit below this line
